a430bf254add437bb6df309ac9b5e3d92a8f5f48
Maksym Honchar <maksim.honchar@gmail.com>
2021-04-12 10:55:11 +0300

    generated array appended in html select

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+const select = document.querySelector('select')
+
 const alphabet = [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 ]
-
 
 let selectOptions = []
 let item
@@ -17,6 +18,14 @@ while (selectOptions.length < 5) {
         selectOptions.push(item)
     }
 }
+
+selectOptions.forEach(optionValue => {
+    const option = optionValue
+    const element = document.createElement('option')
+    element.textContent = option.toUpperCase()
+    element.value = option
+    select.appendChild(element)
+})
 
 console.log(selectOptions)
 
