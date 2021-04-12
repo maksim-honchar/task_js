344b44c0ebc3ae93d4e1994874aa11153162dcf3
Maksym Honchar <maksim.honchar@gmail.com>
2021-04-12 17:16:04 +0300

    logical part completed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-const select = document.querySelector('select')
+import list from './list.json'
+
+const select = document.getElementById('select')
+const listContainer = document.getElementById('list-container')
 
 const alphabet = [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
@@ -27,9 +30,31 @@ selectOptions.forEach(optionValue => {
     select.appendChild(element)
 })
 
-console.log(selectOptions)
+const clearList = listContainer => {
+    while (listContainer.firstChild) {
+        listContainer.removeChild(listContainer.firstChild)
+    }
+}
 
+const stubText = () => {
+    const para = document.createElement('p')
+    para.innerText = 'No matches found'
+    document.getElementById('list-container').appendChild(para)
+}
 
+select.addEventListener('change', ({ target: { value } }) => {
+    clearList(listContainer)
 
+    list
+        .filter(({ name }) => name.substring(0, 1).toLowerCase() === value.toLowerCase())
+        .forEach(({ name }) => {
+            const para = document.createElement('p')
+            para.innerText = name
+            document.getElementById('list-container').appendChild(para)
+        })
 
+    if (!listContainer.lastChild) {
+        stubText()
+    }
+})
 
